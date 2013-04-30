0fc840f97be5b495cfcb31b49dc18295f54f5973
prime8 <timbacon+github@gmail.com>
2013-04-30 06:59:07 -0400

    Tidy-up TestRabbitMQUtil
    Remove trailing whitespace
    Fix import sequence and junit.framework import

--- src/test/java/org/apache/flume/TestRabbitMQUtil.java
+++ src/test/java/org/apache/flume/TestRabbitMQUtil.java
@@ -18,11 +18,11 @@
  */
 package org.apache.flume;
 
-import com.rabbitmq.client.ConnectionFactory;
-import junit.framework.Assert;
-import org.junit.Test;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
+import com.rabbitmq.client.ConnectionFactory;
 
 /**
  *
@@ -33,20 +33,20 @@ public class TestRabbitMQUtil {
     public void close_null() {
         RabbitMQUtil.close(null, null);
     }
-    
+
     @Test(expected=java.lang.IllegalArgumentException.class)
     public void getFactory_null(){
         RabbitMQUtil.getFactory(null);
     }
-    
+
     @Test(expected=java.lang.IllegalArgumentException.class)
     public void getFactory_null_hostname(){
         context.put(RabbitMQConstants.CONFIG_HOSTNAME, null);
-        RabbitMQUtil.getFactory(context);  
+        RabbitMQUtil.getFactory(context);
     }
-    
+
     Context context = null;
-    
+
     @Before
     public void createContext() {
         context = new Context();
@@ -57,12 +57,12 @@ public class TestRabbitMQUtil {
         context.put(RabbitMQConstants.CONFIG_USERNAME, "asdfasdfasd");
         context.put(RabbitMQConstants.CONFIG_VIRTUALHOST, "virtualhost1");
     }
-    
+
     @Test
-    public void getFactory(){        
+    public void getFactory(){
         ConnectionFactory factory = RabbitMQUtil.getFactory(context);
         Assert.assertNotNull("factory should not be null", context);
-        
+
         Assert.assertEquals("Host does not match", context.getString(RabbitMQConstants.CONFIG_HOSTNAME), factory.getHost());
         Assert.assertEquals("Port does not match", context.getInteger(RabbitMQConstants.CONFIG_PORT), (Integer)factory.getPort());
         Assert.assertEquals("ConnectionTimeout does not match", context.getInteger(RabbitMQConstants.CONFIG_CONNECTIONTIMEOUT), (Integer)factory.getConnectionTimeout());
@@ -70,15 +70,15 @@ public class TestRabbitMQUtil {
         Assert.assertEquals("Username does not match", context.getString(RabbitMQConstants.CONFIG_USERNAME), factory.getUsername());
         Assert.assertEquals("VirtualHost does not match", context.getString(RabbitMQConstants.CONFIG_VIRTUALHOST), factory.getVirtualHost());
     }
-    
+
     public void getFactory_minimal(){
         context = new Context();
         context.put(RabbitMQConstants.CONFIG_HOSTNAME, "server01.example.com");
-        
+
         ConnectionFactory factory = RabbitMQUtil.getFactory(context);
         Assert.assertNotNull("factory should not be null", context);
-        
+
         Assert.assertEquals("Host does not match", context.getString(RabbitMQConstants.CONFIG_HOSTNAME), factory.getHost());
     }
-    
+
 }
