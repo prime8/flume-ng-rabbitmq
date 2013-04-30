4728919e86b78dbd510d57fb1b466450dc185b38
prime8 <timbacon+github@gmail.com>
2013-04-30 07:58:11 -0400

    Fix-up whitespace/unused imports in RabbitMQSink
    Also able to make _CounterGroup field final

--- src/main/java/org/apache/flume/sink/rabbitmq/RabbitMQSink.java
+++ src/main/java/org/apache/flume/sink/rabbitmq/RabbitMQSink.java
@@ -19,43 +19,48 @@
 
 package org.apache.flume.sink.rabbitmq;
 
-import com.rabbitmq.client.BasicProperties;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.impl.AMQBasicProperties;
-import org.apache.flume.*;
+import org.apache.flume.Context;
+import org.apache.flume.CounterGroup;
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.RabbitMQConstants;
+import org.apache.flume.RabbitMQUtil;
+import org.apache.flume.Transaction;
 import org.apache.flume.conf.Configurable;
 import org.apache.flume.sink.AbstractSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 public class RabbitMQSink extends AbstractSink implements Configurable {
     private static final Logger log = LoggerFactory.getLogger(RabbitMQSink.class);
-    private CounterGroup _CounterGroup;
+    private final CounterGroup _CounterGroup;
     private ConnectionFactory _ConnectionFactory;
     private Connection _Connection;
     private Channel _Channel;
     private String _QueueName;
     private String _ExchangeName;
-    
+
     public RabbitMQSink(){
         _CounterGroup=new CounterGroup();
     }
-    
+
     @Override
     public void configure(Context context) {
         _ConnectionFactory = RabbitMQUtil.getFactory(context);
         _QueueName = RabbitMQUtil.getQueueName(context);
-        _ExchangeName= RabbitMQUtil.getExchangeName(context);        
+        _ExchangeName= RabbitMQUtil.getExchangeName(context);
     }
 
     @Override
     public synchronized void stop() {
-        RabbitMQUtil.close(_Connection, _Channel);      
+        RabbitMQUtil.close(_Connection, _Channel);
         super.stop();
     }
-    
+
     private void resetConnection(){
         _CounterGroup.incrementAndGet(RabbitMQConstants.COUNTER_EXCEPTION);
         if(log.isWarnEnabled())log.warn(this.getName() + " - Closing RabbitMQ connection and channel due to exception.");
@@ -63,47 +68,47 @@ public class RabbitMQSink extends AbstractSink implements Configurable {
         _Connection=null;
         _Channel=null;
     }
-    
+
     @Override
     public Status process() throws EventDeliveryException {
         if(null==_Connection){
             try {
                 if(log.isInfoEnabled())log.info(this.getName() + " - Opening connection to " + _ConnectionFactory.getHost() + ":" + _ConnectionFactory.getPort());
                 _Connection = _ConnectionFactory.newConnection();
-                _CounterGroup.incrementAndGet(RabbitMQConstants.COUNTER_NEW_CONNECTION);               
+                _CounterGroup.incrementAndGet(RabbitMQConstants.COUNTER_NEW_CONNECTION);
                 _Channel = null;
             } catch(Exception ex) {
                 if(log.isErrorEnabled()) log.error(this.getName() + " - Exception while establishing connection.", ex);
                 resetConnection();
                 return Status.BACKOFF;
-            }            
+            }
         }
-        
+
         if(null==_Channel){
             try {
                 if(log.isInfoEnabled())log.info(this.getName() + " - creating channel...");
                 _Channel = _Connection.createChannel();
                 _CounterGroup.incrementAndGet(RabbitMQConstants.COUNTER_NEW_CHANNEL);
                 if(log.isInfoEnabled())log.info(this.getName() + " - Connected to " + _ConnectionFactory.getHost() + ":" + _ConnectionFactory.getPort());
-            } catch(Exception ex) {              
+            } catch(Exception ex) {
                 if(log.isErrorEnabled()) log.error(this.getName() + " - Exception while creating channel.", ex);
                 resetConnection();
                 return Status.BACKOFF;
-            }             
+            }
         }
-        
+
         Transaction tx = getChannel().getTransaction();
 
         try {
             tx.begin();
-            
+
             Event e = getChannel().take();
 
             if(e==null){
                 tx.rollback();
                 return Status.BACKOFF;
             }
-            
+
             try {
                 _Channel.basicPublish(_ExchangeName, _QueueName, null, e.getBody());
                 tx.commit();
@@ -112,20 +117,20 @@ public class RabbitMQSink extends AbstractSink implements Configurable {
                 resetConnection();
                 throw ex;
             }
-            
+
             return Status.READY;
 
         } catch (Exception ex) {
-         
+
           tx.rollback();
-          
+
           if(log.isErrorEnabled())
               log.error(this.getName() + " - Exception while publishing...", ex);
-          
+
           return Status.BACKOFF;
 
         } finally {
             tx.close();
-        }   
-    }   
+        }
+    }
 }
