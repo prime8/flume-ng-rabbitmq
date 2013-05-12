f7ada9a326bbf35e32883c190b0030a9a9eba441
prime8 <timbacon+github@gmail.com>
2013-05-12 08:16:30 -0400

    Add test for flume event headers

--- src/test/java/org/apache/flume/source/rabbitmq/TestRabbitMQSource.java
+++ src/test/java/org/apache/flume/source/rabbitmq/TestRabbitMQSource.java
@@ -6,6 +6,10 @@ import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.flume.CounterGroup;
 import org.apache.flume.Event;
 import org.apache.flume.PollableSource;
@@ -27,7 +31,6 @@ import com.rabbitmq.client.GetResponse;
 public class TestRabbitMQSource {
 
     private static final String NO_QUEUE_NAME = "", NO_EXCHANGE_NAME = "";
-    private static final String[] NO_TOPICS = {};
     private static final byte[] RABBIT_MESSAGE_BODY = "the body".getBytes();
     private static final Long RABBIT_MESSAGE_ID = 987654321L;
 
@@ -40,6 +43,7 @@ public class TestRabbitMQSource {
     @Mock private GetResponse rabbitMessage;
     @Mock private BasicProperties rabbitMessageProperties;
     @Mock private Envelope rabbitMessageEnvelope;
+    @Mock private Map<String, Object> rabbitMessageHeaders;
 
     @Before
     public void setUp() throws Exception {
@@ -47,21 +51,36 @@ public class TestRabbitMQSource {
         when(connectionFactory.getHost()).thenReturn("NoHost");
     }
 
+    private RabbitMQSource setUpSource(String queue, String exchange, String... topics) {
+        RabbitMQSource source = new RabbitMQSource(counterGroup, connectionFactory,
+                queue, exchange, topics);
+        source.setName(getClass().getSimpleName());
+        source.setChannelProcessor(channelProcessor);
+        return source;
+    }
+
+    private void expectSuccessfulConnection() throws IOException {
+        when(connectionFactory.newConnection()).thenReturn(connection);
+        when(connection.createChannel()).thenReturn(channel);
+    }
+
+    private void expectRabbitMessageReceived(Map<String, Object> headers,
+            byte[] body, long deliveryTag) throws IOException {
+        when(channel.basicGet(NO_QUEUE_NAME, false)).thenReturn(rabbitMessage);
+        when(rabbitMessage.getProps()).thenReturn(rabbitMessageProperties);
+        when(rabbitMessageProperties.getHeaders()).thenReturn(headers);
+        when(rabbitMessage.getBody()).thenReturn(body);
+        when(rabbitMessage.getEnvelope()).thenReturn(rabbitMessageEnvelope);
+        when(rabbitMessageEnvelope.getDeliveryTag()).thenReturn(deliveryTag);
+    }
+
     @Test
     public void simplestHappyPathShouldGetRabbitMessageAndCreateFlumeEventFromIt()
             throws Exception {
-        RabbitMQSource simplestSource = new RabbitMQSource(counterGroup, connectionFactory,
-                NO_QUEUE_NAME, NO_EXCHANGE_NAME, NO_TOPICS);
-        simplestSource.setName(getClass().getSimpleName());
-        simplestSource.setChannelProcessor(channelProcessor);
+        RabbitMQSource simplestSource = setUpSource(NO_QUEUE_NAME, NO_EXCHANGE_NAME);
 
-        when(connectionFactory.newConnection()).thenReturn(connection);
-        when(connection.createChannel()).thenReturn(channel);
-        when(channel.basicGet(NO_QUEUE_NAME, false)).thenReturn(rabbitMessage);
-        when(rabbitMessage.getProps()).thenReturn(rabbitMessageProperties);
-        when(rabbitMessage.getBody()).thenReturn(RABBIT_MESSAGE_BODY);
-        when(rabbitMessage.getEnvelope()).thenReturn(rabbitMessageEnvelope);
-        when(rabbitMessageEnvelope.getDeliveryTag()).thenReturn(RABBIT_MESSAGE_ID);
+        expectSuccessfulConnection();
+        expectRabbitMessageReceived(rabbitMessageHeaders, RABBIT_MESSAGE_BODY, RABBIT_MESSAGE_ID);
 
         assertThat(simplestSource.process(), is(PollableSource.Status.READY));
 
@@ -81,6 +100,50 @@ public class TestRabbitMQSource {
             public void describeTo(Description description) {
                 description.appendText("eventWithBody [")
                     .appendValue(new String(body))
+                    .appendText("]");
+            }
+        });
+    }
+
+    @Test
+    public void flumeEventHeadersShouldContainLowercasedRabbitMessageHeadersAndTimestamp()
+            throws Exception {
+        RabbitMQSource simplestSource = setUpSource(NO_QUEUE_NAME, NO_EXCHANGE_NAME);
+
+        expectSuccessfulConnection();
+        Map<String, Object> fooHeaders = new HashMap<String, Object>();
+        fooHeaders.put("Foo", "Bar");
+        expectRabbitMessageReceived(fooHeaders, RABBIT_MESSAGE_BODY, RABBIT_MESSAGE_ID);
+
+        assertThat(simplestSource.process(), is(PollableSource.Status.READY));
+
+        Map<String, String> eventHeaders = new HashMap<String, String>();
+        eventHeaders.put("foo", "Bar"); // key is lowercased
+        verify(channelProcessor).processEvent(withEventWithHeaders(eventHeaders ));
+        verify(channel).basicAck(RABBIT_MESSAGE_ID, false);
+    }
+
+    private Event withEventWithHeaders(final Map<String, String> headers) {
+        return argThat(new BaseMatcher<Event>() {
+            @Override
+            public boolean matches(Object actual) {
+                return actual != null
+                        && actual instanceof Event
+                        && hasHeaders(((Event)actual).getHeaders(),headers);
+            }
+            private boolean hasHeaders(Map<String, String> actual, Map<String, String> expected) {
+                for (Map.Entry<String, String> entry: expected.entrySet()) {
+                    if (!actual.containsKey(entry.getKey())) return false;
+                    if (!actual.get(entry.getKey()).equals(entry.getValue())) return false;
+                }
+                // timestamp header is added if not present
+                return actual.containsKey("timestamp")
+                        && Long.valueOf(actual.get("timestamp")) > 0;
+            }
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("eventWithHeaders [")
+                    .appendValue(headers)
                     .appendText("]");
             }
         });
